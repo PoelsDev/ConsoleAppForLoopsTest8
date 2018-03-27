568af38c4c86f539ec7c4c21b36db836c5171f7b
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:04:38 +0200

    Program

--- ConsoleAppForLoopsTest8/Program.cs
+++ ConsoleAppForLoopsTest8/Program.cs
@@ -4,9 +4,26 @@ namespace ConsoleAppForLoopsTest8
 {
     class Program
     {
+        static Random rndGen = new Random();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int aantalLines = 8;
+
+            for (var i = 0; i < aantalLines; i++)
+            {
+                int aantalTekens = rndGen.Next(20, 31);
+                TekenLijn(aantalTekens);            
+            }
+        }
+
+        static void TekenLijn(int lengte)
+        {
+            for (var j = 0; j < lengte; j++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
         }
     }
 }
